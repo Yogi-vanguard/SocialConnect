c2d61bada897932ba25e967c680ac91277716ae9
yogendrasinghrawat <yogi7work@gmail.com>
2022-05-21 13:43:26 +0530

    project, Review, Tag

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from projects.variables.constant import Constants
 
 # Create your views here.
+def projects(request):
+    msg = "Hello you are on the project page"
+    context = {"message":msg}
+    return render(request, 'projects/projects.html',context)
+
+
+def project(request, pk):
+    msg = "Hello you are on the project page"
+    context = {"message":msg}
+    return render(request, 'projects/single-project.html')
--- socialconnect/urls.py
+++ socialconnect/urls.py
@@ -14,21 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-
-def projects(request):
-    return HttpResponse("Here are our products ")
-
-
-def project(request, pk):
-    return HttpResponse("single project")
-
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('projects/', projects, name="projects"),
-    path('project/<str:pk>/', project, name="project"),
-
+    path('',include('projects.urls')),
 ]
