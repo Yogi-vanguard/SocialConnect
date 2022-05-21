97f6909cc010e365c1bf30435bb3613fc0bb713b
yogendrasinghrawat <yogi7work@gmail.com>
2022-05-21 13:51:31 +0530

    made changes to Project model and migrate those changes

--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Project
+from . import models
 # Register your models here.
 
-admin.site.register(Project)
+admin.site.register(models.Project)
+admin.site.register(models.Review)
+admin.site.register(models.Tag)
--- projects/models.py
+++ projects/models.py
@@ -11,6 +11,7 @@ class Project(models.Model):
     source_link = models.CharField(max_length=2000, null=True, blank=True)
     tags = models.ManyToManyField('Tag',blank=True)
     vote_total = models.IntegerField(default=0,null=True,blank=True)
+    vote_ratio = models.IntegerField(default=0,null=True,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False)
 
