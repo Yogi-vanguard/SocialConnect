ce6c3aabfd5be40edd6818e90883b78b5c503232
yogendrasinghrawat <yogi7work@gmail.com>
2022-05-23 00:15:10 +0530

    added static files and theme installation

--- projects/forms.py
+++ projects/forms.py
@@ -1,8 +1,19 @@
 from dataclasses import field
-from django.forms import ModelForm
+from tkinter import Widget
+from django import forms
+from django.forms import ModelForm, widgets
 from .models import Project
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title','description','tags','demo_link','source_link','featured_image']
+        widgets = {
+            'tags':forms.CheckboxSelectMultiple(),
+        }
+    
+    def __init__(self,*args,**kwrgs):
+        super(ProjectForm,self).__init__(*args,**kwrgs)
+        for name,field in self.fields.items():
+            field.widget.attrs.update({'class':'input'})
+        # self.fields['title'].widget.attrs.update({'class':'input'})
